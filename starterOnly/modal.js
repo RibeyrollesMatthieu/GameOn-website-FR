9a2dcc912fdf1b2094588331e7135a9d6637eefc
MatthieuRibeyrolles <ribeyrolles.matthieu@gmail.com>
2022-11-07 11:04:31 +0100

    fix #1: modal now closing on click

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,26 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
   } else {
-    x.className = "topnav";
+    x.className = 'topnav';
   }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector('.bground');
+const modalBtn = document.querySelectorAll('.modal-btn');
+const formData = document.querySelectorAll('.formData');
+const modalCloseButton = document.querySelector('.close');
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = 'block';
 }
 
-
+/* Function 'closing' the modal on click */
+/* prettier-ignore */
+modalCloseButton.addEventListener('click', () => modalbg.style.display = 'none');
